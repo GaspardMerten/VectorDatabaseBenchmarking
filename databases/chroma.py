8aa6bfc9ebec09aedd6778a8f4645e451e0144fd
sami73232@gmail.com <el.aloui.sami@ulb.be>
2023-11-21 17:05:32 +0100

    problem test

--- databases/chroma.py
+++ databases/chroma.py
@@ -1,20 +1,62 @@
-from typing import List
+from typing import List, Iterable
+from chromadb import ChromaDB
 
-from .base import AVectorDatabase, Vector
+Vector = List[float]
 
+class ChromaVectorDatabase:
+    """
+    ChromaDB implementation of the vector database.
 
-class Chroma(AVectorDatabase):
+    This class interacts with ChromaDB for efficient vector operations.
+    """
+
     def __init__(self, name: str, vector_dim: int) -> None:
-        pass
+        """
+        Initialize the ChromaDB database, create a collection, etc.
+        """
+        self.db = ChromaDB(name)
+        self.collection = self.db.create_collection(name, dimension=vector_dim)
 
-    def batch_upsert(self, vectors: List[Vector]) -> None:
-        pass
+    def batch_upsert(self, vectors: Iterable) -> None:
+        """
+        Upsert a batch of vectors using ChromaDB.
+        """
+        for vector in vectors:
+            self.collection.insert([vector])
 
     def upsert(self, vector: Vector) -> None:
-        pass
+        """
+        Upsert a single vector using ChromaDB.
+        """
+        self.batch_upsert([vector])
 
     def query(self, vector: Vector, top_k: int) -> List[str]:
-        pass
+        """
+        Query the database for similar vectors using ChromaDB, top-k.
+        """
+        result = self.collection.query(vector, top_k)
+        return [str(item["_id"]) for item in result]
 
     def reset(self) -> None:
+        """
+        Reset the database, delete the collection.
+        """
+        self.db.drop_collection(self.collection.name)
+
+    def create_index(self) -> None:
+        """
+        ChromaDB doesn't use traditional indexes, so this method might not be applicable.
+        """
         pass
+
+    def close(self) -> None:
+        """
+        Close the connection to the database.
+        """
+        pass  # ChromaDB doesn't require explicit closing
+
+    def get_storage_size(self) -> int:
+        """
+        Get the storage size of the database.
+        """
+        return 0  # ChromaDB may not provide direct access to storage size information
