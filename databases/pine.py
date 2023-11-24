51ae1c71704a350d34123e3525084450ced84361
Rhody <45152539+KingRhody@users.noreply.github.com>
2023-11-24 02:29:32 +0100

    Update pine.py
    Added definition to functions

--- databases/pine.py
+++ databases/pine.py
@@ -1,20 +1,45 @@
 from typing import List
+import pinecone
 
 from .base import AVectorDatabase, Vector
 
 
 class Pine(AVectorDatabase):
     def __init__(self, name: str, vector_dim: int) -> None:
-        pass
+        """
+        Initialize the Pinecone database, create an index, etc.
+        """
+        pinecone.create_index(name, dimension=vector_dim, metric="euclidean")
+        pinecone.describe_index(name)
 
     def batch_upsert(self, vectors: List[Vector]) -> None:
-        pass
+        for vector in vectors:
+            self.upsert(vector)
 
     def upsert(self, vector: Vector) -> None:
-        pass
+        self.upsert(vector)
 
     def query(self, vector: Vector, top_k: int) -> List[str]:
-        pass
+        """
+        Query the database for similar vectors, top-k.
 
-    def reset(self) -> None:
-        pass
+        Args:
+            vector (Vector): Vector to query
+            top_k (int): Number of results to return
+
+        Returns:
+            List[str]: List of ids of the top-k results
+        """
+        self.query(
+            vector=vector,
+            top_k=top_k,
+            include_values=True
+        )
+
+    def reset(self, name: str) -> None:
+        """
+        :param name: name of the index
+        :return: None
+        Delete the index called 'name'
+        """
+        pinecone.delete_index(name)
