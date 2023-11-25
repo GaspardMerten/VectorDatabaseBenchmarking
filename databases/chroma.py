ec64eebf5fdecc83efae2e302aacee9c9dd7d5ee
RedhwaneBS <99534301+RedhwaneBS@users.noreply.github.com>
2023-11-25 16:38:24 +0100

    Add files via upload

--- databases/chroma.py
+++ databases/chroma.py
@@ -1,9 +1,10 @@
-from typing import List, Iterable
-from chromadb import ChromaDB
+from base import AVectorDatabase
+import chromadb
+from typing import List
 
 Vector = List[float]
 
-class ChromaVectorDatabase:
+class ChromaVectorDatabase(AVectorDatabase):
     """
     ChromaDB implementation of the vector database.
 
@@ -12,51 +13,57 @@ class ChromaVectorDatabase:
 
     def __init__(self, name: str, vector_dim: int) -> None:
         """
-        Initialize the ChromaDB database, create a collection, etc.
+        Initialize the ChromaDB database, create a collection.
         """
-        self.db = ChromaDB(name)
-        self.collection = self.db.create_collection(name, dimension=vector_dim)
+        self.chroma_client = chromadb.Client(settings = chromadb.Settings(allow_reset=True))
+        self.vector_dim = vector_dim
+        self.collection_name = name
+        self.collection = self.chroma_client.create_collection(name=name)
 
-    def batch_upsert(self, vectors: Iterable) -> None:
-        """
-        Upsert a batch of vectors using ChromaDB.
-        """
-        for vector in vectors:
-            self.collection.insert([vector])
-
-    def upsert(self, vector: Vector) -> None:
+    def upsert(self, vector: Vector, id: str) -> None:
         """
         Upsert a single vector using ChromaDB.
         """
-        self.batch_upsert([vector])
+        self.collection.add( embeddings=vector, ids=id)
+
+    def batch_upsert(self, vectors: List[Vector], ids: List[str]) -> None:
+        """
+        Upsert a batch of vectors using ChromaDB.
+        """
+        for vector, id in zip(vectors, ids):
+            self.upsert(vector, id)
+        
 
     def query(self, vector: Vector, top_k: int) -> List[str]:
         """
         Query the database for similar vectors using ChromaDB, top-k.
         """
-        result = self.collection.query(vector, top_k)
-        return [str(item["_id"]) for item in result]
+        return self.collection.query(
+            n_results=top_k,
+            query_embeddings=vector
+            )
+    
+
 
     def reset(self) -> None:
         """
         Reset the database, delete the collection.
         """
-        self.db.drop_collection(self.collection.name)
+        self.chroma_client.reset()
+
 
     def create_index(self) -> None:
         """
-        ChromaDB doesn't use traditional indexes, so this method might not be applicable.
+        Create an index on the vector column.
         """
         pass
 
     def close(self) -> None:
         """
-        Close the connection to the database.
+        Close the database connection.
         """
-        pass  # ChromaDB doesn't require explicit closing
+        pass
 
     def get_storage_size(self) -> int:
-        """
-        Get the storage size of the database.
-        """
-        return 0  # ChromaDB may not provide direct access to storage size information
+        pass
+
